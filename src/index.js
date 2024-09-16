ac217ce2ad764f2e3da995aecbddbf6b0f8bdf07
João Pedro Silva Caglianone <89998145+jpCaglianone@users.noreply.github.com>
2024-09-16 10:45:31 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './App.css';
 import App from './App';
-// import Desativado from './desativado';
+import Desativado from './desativado';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// const __WORKING = true
+const __WORKING = true
 
-// if (!__WORKING) {
-//   root.render(
-//     <React.StrictMode>
-//       <Desativado />
-//     </React.StrictMode>
-//   );
-// }
-// else {
+if (!__WORKING) {
+  root.render(
+    <React.StrictMode>
+      <Desativado />
+    </React.StrictMode>
+  );
+}
+else {
   root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
   )
-   //;}
+   ;}
 
 
